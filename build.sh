72e3f0875b03d42e5256dcfdbc41a3286e87793e
Maarten <maartenvanbeek@gmail.com>
2012-07-21 15:18:33 +0200

    Added messages

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
 cd smalltalk
-autoreconf -vi && ./configure && make      #Building 32-bit
 
+echo ""
+echo ""
+echo ""
+echo "########################"
+echo "#    BUILDING 32-BIT   #"
+echo "########################"
+echo ""
+echo ""
+echo ""
+autoreconf -vi && ./configure && make      #Building 32-bit
+echo ""
+echo ""
+echo ""
+echo "########################"
+echo "#    BUILDING 32-BIT   #"
+echo "########################"
+echo ""
+echo ""
+echo ""
 export CFLAGS="$CFLAGS -m64"
 autoreconf -vi && ./configure && make      #Building 64-bit
